2e2b672cf928b84463d8af5f29a3167fd5b4286a
Keisuke Taniguchi <rememberthatk@icloud.com>
2021-07-27 22:02:53 +0900

    [master] add document getUsers

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <Tutorial/>
+  <div>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { defineComponent } from '@vue/composition-api'
+import { GetUsersDocument } from '@/generated/graphql'
+import { useQuery, useResult } from '@vue/apollo-composable'
 
-export default Vue.extend({})
+export default defineComponent({
+  name: 'IndexPage',
+  setup() {
+    const { result } = useQuery(GetUsersDocument)
+    const users = useResult(result, [], (data) => data?.users)
+
+    return { users }
+  },
+})
 </script>
+
+<style lang="scss" scoped>
+</style>
+
